8ea4cc4d384794ce6b9905d76c941dc0f71baff9
Ivon Ferreira <ivonferreira@users.noreply.github.com>
2023-01-18 11:20:48 -0300

    Corrigindo e concluindo Exercicio

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,161 +1,33 @@
-let bt1 = document.getElementById('rate1')
-let bt2 = document.getElementById('rate2')
-let bt3 = document.getElementById('rate3')
-let bt4 = document.getElementById('rate4')
-let bt5 = document.getElementById('rate5')
-let score = document.querySelector('span#score')
-let before = document.querySelector('div#before')
-let after = document.querySelector('div#after')
+let bt1 = document.getElementById('rate1');
+let bt2 = document.getElementById('rate2');
+let bt3 = document.getElementById('rate3');
+let bt4 = document.getElementById('rate4');
+let bt5 = document.getElementById('rate5');
+let score = document.querySelector('span#score');
+let before = document.querySelector('div#before');
+let after = document.querySelector('div#after');
 
-function rate1(){
-    if (bt1.value == "OFF"){
-        bt1.value = "ON";
-        bt1.style.backgroundColor = '#959eac';
-        bt1.style.color = '#ffffff'
-        bt2.value = "OFF";
-        bt2.style.backgroundColor = "#3a4655";
-        bt3.value = "OFF";
-        bt3.style.backgroundColor = "#3a4655";
-        bt4.value = "OFF";
-        bt4.style.backgroundColor = "#3a4655";
-        bt5.value = "OFF";
-        bt5.style.backgroundColor = "#3a4655";
-
+function rate(n=0){
+    let buttons = [bt1, bt2, bt3, bt4, bt5]
+    for (let c in buttons){
+        if (c == n ){
+            if (buttons[n].value == "OFF"){
+                buttons[n].value = "ON";
+                buttons[n].style.backgroundColor = '#959eac';
+                buttons[n].color = '#ffffff';
+        }else{
+            buttons[n].value = "OFF";
+            buttons[n].style.backgroundColor = "#3a4655";
+            buttons[n].style.color = '#959eac';
+        }
     }else{
-        bt1.value = "OFF";
-        bt1.style.backgroundColor = "#3a4655";
-        bt1.style.color = '#959eac';
-        bt2.value = "OFF";
-        bt2.style.backgroundColor = "#3a4655";
-        bt3.value = "OFF";
-        bt3.style.backgroundColor = "#3a4655";
-        bt4.value = "OFF";
-        bt4.style.backgroundColor = "#3a4655";
-        bt5.value = "OFF";
-        bt5.style.backgroundColor = "#3a4655";
-        
+        buttons[c].value = "OFF";
+        buttons[c].style.backgroundColor = "#3a4655";
+        buttons[c].style.color = '#959eac';
+        }
     }
-  
-}
-function rate2(){
-    if (bt2.value == "OFF"){
-        bt2.value = "ON";
-        bt2.style.backgroundColor = '#959eac';
-        bt2.style.color = '#ffffff'
-        bt1.value = "OFF";
-        bt1.style.backgroundColor = "#3a4655";
-        bt3.value = "OFF";
-        bt3.style.backgroundColor = "#3a4655";
-        bt4.value = "OFF";
-        bt4.style.backgroundColor = "#3a4655";
-        bt5.value = "OFF";
-        bt5.style.backgroundColor = "#3a4655";
-
-    }else{
-        bt1.value = "OFF";
-        bt1.style.backgroundColor = "#3a4655";
-        bt2.style.color = '#959eac';
-        bt2.value = "OFF";
-        bt2.style.backgroundColor = "#3a4655";
-        bt3.value = "OFF";
-        bt3.style.backgroundColor = "#3a4655";
-        bt4.value = "OFF";
-        bt4.style.backgroundColor = "#3a4655";
-        bt5.value = "OFF";
-        bt5.style.backgroundColor = "#3a4655";
         
-    }
-  
-}
-function rate3(){
-    if (bt3.value == "OFF"){
-        bt3.value = "ON";
-        bt3.style.backgroundColor = '#959eac';
-        bt3.style.color = '#ffffff'
-        bt2.value = "OFF";
-        bt2.style.backgroundColor = "#3a4655";
-        bt1.value = "OFF";
-        bt1.style.backgroundColor = "#3a4655";
-        bt4.value = "OFF";
-        bt4.style.backgroundColor = "#3a4655";
-        bt5.value = "OFF";
-        bt5.style.backgroundColor = "#3a4655";
-
-    }else{
-        bt1.value = "OFF";
-        bt1.style.backgroundColor = "#3a4655";
-        bt3.style.color = '#959eac';
-        bt2.value = "OFF";
-        bt2.style.backgroundColor = "#3a4655";
-        bt3.value = "OFF";
-        bt3.style.backgroundColor = "#3a4655";
-        bt4.value = "OFF";
-        bt4.style.backgroundColor = "#3a4655";
-        bt5.value = "OFF";
-        bt5.style.backgroundColor = "#3a4655";
-        
-    }
-  
-}
-function rate4(){
-    if (bt4.value == "OFF"){
-        bt4.value = "ON";
-        bt4.style.backgroundColor = '#959eac';
-        bt4.style.color = '#ffffff'
-        bt2.value = "OFF";
-        bt2.style.backgroundColor = "#3a4655";
-        bt3.value = "OFF";
-        bt3.style.backgroundColor = "#3a4655";
-        bt1.value = "OFF";
-        bt1.style.backgroundColor = "#3a4655";
-        bt5.value = "OFF";
-        bt5.style.backgroundColor = "#3a4655";
-
-    }else{
-        bt1.value = "OFF";
-        bt1.style.backgroundColor = "#3a4655";
-        bt4.style.color = '#959eac';
-        bt2.value = "OFF";
-        bt2.style.backgroundColor = "#3a4655";
-        bt3.value = "OFF";
-        bt3.style.backgroundColor = "#3a4655";
-        bt4.value = "OFF";
-        bt4.style.backgroundColor = "#3a4655";
-        bt5.value = "OFF";
-        bt5.style.backgroundColor = "#3a4655";
-        
-    }
-  
-}
-function rate5(){
-    if (bt5.value == "OFF"){
-        bt5.value = "ON";
-        bt5.style.backgroundColor = '#959eac';
-        bt5.style.color = '#ffffff'
-        bt2.value = "OFF";
-        bt2.style.backgroundColor = "#3a4655";
-        bt3.value = "OFF";
-        bt3.style.backgroundColor = "#3a4655";
-        bt4.value = "OFF";
-        bt4.style.backgroundColor = "#3a4655";
-        bt1.value = "OFF";
-        bt1.style.backgroundColor = "#3a4655";
-
-    }else{
-        bt1.value = "OFF";
-        bt1.style.backgroundColor = "#3a4655";
-        bt5.style.color = '#959eac';
-        bt2.value = "OFF";
-        bt2.style.backgroundColor = "#3a4655";
-        bt3.value = "OFF";
-        bt3.style.backgroundColor = "#3a4655";
-        bt4.value = "OFF";
-        bt4.style.backgroundColor = "#3a4655";
-        bt5.value = "OFF";
-        bt5.style.backgroundColor = "#3a4655";
-        
-    }
-  
+    
 }
 
 function hover(x){
